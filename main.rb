356ef2f233ae9c9e828ef2bc910e576bbda9ce78
Christine Coulter <christinetcoulter@gmail.com>
2013-06-26 12:04:11 -0400

    update with david's stuff

--- main.rb
+++ main.rb
@@ -5,3 +5,54 @@ require 'sinatra/reloader' if development?
 get '/' do
   erb :home
 end
+
+get '/about' do
+  "This is me"
+end
+
+get '/new_link' do
+  erb :new_link
+end
+
+post '/new_link' do
+  @link_name = params[:name]
+  @link = params[:link]
+  f = File.new('links.csv', 'a+')
+  f.puts("#{@link_name},#{@link}")
+  f.close
+  redirect to("/links")
+end
+
+get '/links' do
+  link_file = File.new('links.csv', 'r')
+  @links = []
+  link_file.each do |line|
+    @links << line.split(',')
+  end
+  link_file.close
+  erb :links
+end
+
+post '/navigate' do
+  case params[:destination].downcase
+    when "links" then redirect to("/links")
+    when "about" then redirect to ("/about")
+  end
+  redirect to("/")
+end
+
+
+
+
+
+
+
+
+# get '/manual_links' do
+#   @links = ['http://reddit.com',
+#             'http://news.ycombinator.com',
+#             'http://techcrunch.com',
+#             'http://macrumors.com',
+#             'http://gizmodo.com']
+#   erb :links
+# end
